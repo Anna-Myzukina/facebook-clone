a9080315f6258ce9d1b911c09e2226151a57df38
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-01-14 17:57:33 +0200

    Generated user and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200114154554) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        :body => "MyText"
+      ),
+      Post.create!(
+        :body => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
