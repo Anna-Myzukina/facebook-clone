1b78ff87795e46684f8cbdc58e3226cf774062b7
Elukoye <lukoyedith@gmail.com>
2020-01-17 18:56:33 +0300

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200117141930) do
   end
 
   create_table "users", force: :cascade do |t|
+    
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
