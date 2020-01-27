42422b2eefbfd50c68f9e6679461a07c47ab3766
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-01-27 15:58:06 +0200

    Modified registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
- protected
-    def after_sign_up_path_for(resource)
+  protected
+     def after_sign_up_path_for(resource)
+       new_user_session_path
+      end
+  
+     def after_inactive_sign_up_path_for(resource)
       new_user_session_path
      end
+ end
  
-    def after_inactive_sign_up_path_for(resource)
-     new_user_session_path
-    end
-end
