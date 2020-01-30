e12d501eab94f484046867412826578ec1844c87
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-01-30 10:20:58 +0200

    Fix linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :user_signed_in?, only: %i[create destroy] 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
