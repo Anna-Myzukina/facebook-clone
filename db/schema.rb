837102189f92c0369862d6fc0172b0e0d63ab7f3
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-01-30 16:44:00 +0200

    Fix error with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200123152004) do
+ActiveRecord::Schema.define(version: 20200124211833) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
