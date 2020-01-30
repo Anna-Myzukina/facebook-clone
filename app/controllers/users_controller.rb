ebdccb28366d7bc96779a169a5514a6ca38ef8c4
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-01-30 16:29:52 +0200

    Fix errors with user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :user_signed_in?, only: %i[create destroy] 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
