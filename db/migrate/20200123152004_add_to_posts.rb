9400ebf919459c7a36ac6b33783154e9692455b5
Elukoye <lukoyedith@gmail.com>
2020-02-02 20:14:18 +0300

    modified post index.html.erb

--- db/migrate/20200123152004_add_to_posts.rb
+++ db/migrate/20200123152004_add_to_posts.rb
@@ -2,6 +2,6 @@ class AddToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :user_id, :integer 
     add_column :posts, :post_id, :integer 
-    add_column :posts, :coment_id, :integer 
+    add_column :posts, :comment_id, :integer 
   end
 end
