71f1496ce79134ec19ae0877518161e4c125d61b
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-04 11:10:18 +0200

    Fix error users#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ devise_for :users,
 path: '',
 path_names: {sign_in: 'login', sign_out: 'logout', edit:'profile',sign_up: 'registration'}
 # controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-resources :users, only: [:show]
+resources :users, only: [:show, :index]
 resources :posts
 
   
