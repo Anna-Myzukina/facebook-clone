c6002f8641d154a80c9ff4753984a5c91efd481d
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-05 16:57:38 +0200

    Added styles to post page and make changes in .rubocop.yml

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = current_user.posts
   end
 
   # GET /posts/1
