f80f276bbda2c9855538da07bed518abfa85b4a6
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-06 10:33:46 +0200

    Added style for comments and posts

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200205175826) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comment_id"
     t.string "username"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -38,8 +39,6 @@ ActiveRecord::Schema.define(version: 20200205175826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "post_id"
-    t.integer "coment_id"
   end
 
   create_table "users", force: :cascade do |t|
