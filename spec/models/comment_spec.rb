47db7d5c32d716b84498a81aa05f78aac792191d
Elukoye <lukoyedith@gmail.com>
2020-02-12 18:11:07 +0300

    modified:db/schema.rb,deleted:docs/20200208205321_create_comments.rb, modified:   spec/models/comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it 'is invalid without a username ' do
-    @comment = Comment.new(body: 'great', user_id: 2, post_id: 2, created_at: '2020-02-10 13:51:03', updated_at: '2020-02-10 13:51:03', username: nil)
+    @comment = Comment.new(body: 'great', user_id: 2, post_id: 2, username: nil)
     expect(@comment.save).to eq(false)
   end
   it ' is invalid without a comment body' do
