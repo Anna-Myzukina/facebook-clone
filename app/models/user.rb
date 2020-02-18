12a767d5e63d5e09e7774e028c065f564b8ee4c5
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-18 20:02:29 +0200

    Fix comments error, added buttons on friendship page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
   def likes?(post)
     post.likes.where(user_id: id).any?
   end
