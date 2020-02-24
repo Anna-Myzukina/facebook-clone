a16f2b76200204a9b9133c013fd8853bc5459398
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-24 17:51:38 +0200

    Change buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,8 +8,8 @@ module UsersHelper
     elsif current_user.pending_friends.include?(user)
       link_to 'Cancel request', user_reject_request_path(user), method: :delete, class: 'btn btn-danger mb-2'
     elsif current_user.friend_requests.include?(user)
-      button = (link_to 'Accept', user_accept_request_path(user), class: 'btn btn-primary mr-2')
-      button.concat(link_to('Reject', user_reject_request_path(user), method: :delete, class: 'btn btn-danger'))
+      button = (button_to 'Accept friend request', user_accept_request_path(user), class: 'btn btn-primary mr-2')
+      button.concat(button_to('Reject', user_reject_request_path(user), method: :delete, class: 'btn btn-danger'))
     else
       link_to 'Add new friend', user_friendships_path(user), class: 'btn btn-info mb-2'
     end
