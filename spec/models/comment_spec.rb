c5ff491de739a951a227add41c789e6fbfe0fa9b
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-02-24 19:23:31 +0200

    Fixed conflict between development and fifth-milestone branches

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it 'is invalid without a username ' do
-    @comment = Comment.new(body: 'great', user_id: 2, post_id: 2, username: nil)
+    @comment = Comment.new(content: 'great', user_id: 2, post_id: 2, user: nil)
     expect(@comment.save).to eq(false)
   end
   it ' is invalid without a comment body' do
-    @comment = Comment.new(body: nil)
+    @comment = Comment.new(content: nil)
     expect(@comment.save).to eq(false)
   end
 end
