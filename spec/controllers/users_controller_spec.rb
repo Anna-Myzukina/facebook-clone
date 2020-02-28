9b43a13ba846fcc38eb79b69839784785c708213
Elukoye <lukoyedith@gmail.com>
2020-02-28 14:28:23 +0300

    resolved conflict between 5th milestone branch and development branch

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET index' do
+    it 'renders 200 ok status code' do
+      expect(response.status).to eq(200)
+    end
+  end
 end
