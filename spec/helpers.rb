d26ac713362ff0b4e6810f5eadfde044b2123f8a
Elukoye <lukoyedith@gmail.com>
2020-03-01 15:06:16 +0300

    fixed rubocop errors

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 def sign_up_user
-    visit '/registration'
-    fill_in 'Username', with: ' jane'
-    fill_in 'Sirname', with: 'doe '
-    fill_in 'Email', with: 'jdoe@gmail.com '
-    fill_in 'Password', with: '123456 '
-    click_on 'Sign up'    
+  visit '/registration'
+  fill_in 'Username', with: ' jane'
+  fill_in 'Sirname', with: 'doe '
+  fill_in 'Email', with: 'jdoe@gmail.com '
+  fill_in 'Password', with: '123456 '
+  click_on 'Sign up'
 end
 
 def sign_in_user
-    visit '/login'
-    fill_in 'Email', with: 'jdoe@gmail.com '
-    fill_in 'Password', with: '123456 '
-    click_on 'Log in'
+  visit '/login'
+  fill_in 'Email', with: 'jdoe@gmail.com '
+  fill_in 'Password', with: '123456 '
+  click_on 'Log in'
 end
